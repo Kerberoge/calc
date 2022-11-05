1fd5f6675dd2333acf46a543f55fd7d23fdd0ff3
Kerberoge <sjoerdenjh@gmail.com>
2022-11-05 23:24:47 +0100

    check if too many arguments are provided

--- calc.c
+++ calc.c
@@ -13,6 +13,11 @@ int main ( int argc, char* argv[] )
         printf("Please enter your expression: ");
         scanf("%s", exp);
     }
+    else
+    {
+        printf("Warning: too many arguments provided!\n");
+        exit(1);
+    }
 
 
 
